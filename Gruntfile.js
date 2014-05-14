f402e6f3409deba053477b12422b441d51842a39
13ruce1337 <tagcoop@gmail.com>
2014-05-14 15:20:53 -0400

    changed message for API

--- Gruntfile.js
+++ Gruntfile.js
@@ -15,7 +15,7 @@ module.exports = function(grunt) {
     },
     uglify: {
       options: {
-        banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+        banner: '/*! <%= pkg.name %> build date: <%= grunt.template.today("yyyy-mm-dd") %> */\n'
       },
       build: {
         src: 'src/bin/tymr.js',
